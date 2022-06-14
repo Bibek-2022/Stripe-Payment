2c4e75e7e7170a6008e3b66e5df6292c762dd6dc
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-14 22:53:20 +1000

    minor changes

--- client/src/index.js
+++ client/src/index.js
@@ -16,16 +16,14 @@ function App() {
     description: "Cool car",
   });
 
-  async function handleToken(token, addresses) {
+  const handleToken = async (token, addresses) => {
     const response = await axios.post("/checkout", { token, product });
     const { status } = response.data;
     console.log("Response:", response.data);
-    if (status === "success") {
-      toast("Success! Check email for details", { type: "success" });
-    } else {
-      toast("Something went wrong", { type: "error" });
-    }
-  }
+    status === "success"
+      ? toast("Success! Check email for details", { type: "success" })
+      : toast("Something went wrong", { type: "error" });
+  };
   const photos = [
     {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
