f0beb991a6720087018821aa70dba9b250b37a9a
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-14 22:09:18 +1000

    Rearranged all the files

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,6 @@
 const cors = require("cors");
 const express = require("express");
-const stripe = require("stripe")(
-  "sk_test_51LAXeAFLFde1l2yS0pInP4jJJhLtEnrKQ5ETuEGuKpxw1EjTL9JQRlb5lAlHfqo814iDpB1FsZM97BfPx6HsB3Gw00vBxgxRUP"
-);
+const stripe = require("stripe")("STRIPE_SECRET_KEY");
 const uuid = require("uuid/v4");
 
 const app = express();
@@ -24,7 +22,7 @@ app.post("/checkout", async (req, res) => {
 
     const customer = await stripe.customers.create({
       email: token.email,
-      source: token.id,
+      source: token.id
     });
 
     const idempotency_key = uuid();
@@ -42,12 +40,12 @@ app.post("/checkout", async (req, res) => {
             line2: token.card.address_line2,
             city: token.card.address_city,
             country: token.card.address_country,
-            postal_code: token.card.address_zip,
-          },
-        },
+            postal_code: token.card.address_zip
+          }
+        }
       },
       {
-        idempotency_key,
+        idempotency_key
       }
     );
     console.log("Charge:", { charge });
